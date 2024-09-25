cb64b0a6698667477261c6bd46d9a5bd0529a3f9
TMHong25 <tmhong25@gmail.com>
2024-09-25 14:55:30 +0700

    b4

--- src/users/theme/header/indexHeader.js
+++ src/users/theme/header/indexHeader.js
@@ -1,6 +1,11 @@
 import {memo} from 'react';
+import './styleHeader.scss';
 const Header = () => {
-    return (<h1>Header</h1>  );
+    return (
+        <div className='header__top'>
+        <div className='Container'>Header</div>
+    </div>
+    );
 };
 
 export default memo(Header);
