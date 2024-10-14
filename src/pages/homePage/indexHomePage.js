69eb88e9336ed07c098bb6226cf01a55394c05bb
TMHong25 <tmhong25@gmail.com>
2024-10-14 22:12:25 +0700

    14/10

--- src/pages/homePage/indexHomePage.js
+++ src/pages/homePage/indexHomePage.js
@@ -1,8 +1,82 @@
-import {memo} from 'react';
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+import './styleHomePage.scss';
+import { memo } from 'react';
+import featProducts from './allProducts'; 
+
+import slider1 from './img/slider1.png';
+import slider2 from './img/slider2.png';
+import slider3 from './img/slider3.png';
+import slider4 from './img/slider4.png';
+import slider5 from './img/slider5.png';
+
+
 const HomePage = () => {
+    const responsive = { 
+        superLargeDesktop: { 
+            breakpoint: { max: 4000, min: 3000 }, 
+            items: 5 
+        }, 
+        desktop: { 
+            breakpoint: { max: 3000, min: 1024 }, 
+            items: 4
+        }, 
+        tablet: { 
+            breakpoint: { max: 1024, min: 464 }, 
+            items: 2 
+        }, 
+        mobile: { 
+            breakpoint: { max: 464, min: 0 }, 
+            items: 1 
+        } 
+    }; 
+
+    const sliderItem = [
+        { bgImg : slider1 },
+        { bgImg : slider2 },
+        { bgImg : slider3 },
+        { bgImg : slider4 },
+        { bgImg : slider5 },
+    ];
+
     return (
-    <h1>HomePage</h1>
-    )
+        <>
+            <Carousel responsive={responsive} className='slider'>
+                {
+                    sliderItem.map((item, key) => (
+                        <div 
+                            className='slider_item'
+                            style={{ backgroundImage: `url(${item.bgImg})` }} 
+                            key={key}
+                        ></div>
+                    ))
+                }
+            </Carousel>
+
+            <div className="container">
+                <div className="featured">
+                    <div className="section_title">
+                        <h2>Sản phẩm nổi bật</h2>
+                        {
+                           featProducts[0].products.map((item, key) => (
+                                <div className="products" key={key}>
+                                    <div className="products_img">
+                                        <img src={item.img} alt={item.name} />
+                                    </div>
+                                    <div className="products_name">
+                                        <h4>{item.name}</h4>
+                                    </div>
+                                    <div className="products_price">
+                                        <span>{item.price}</span>
+                                    </div>
+                                </div>
+                            ))
+                        }
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 };
 
 export default memo(HomePage);
--- src/pages/theme/header/indexHeader.js
+++ src/pages/theme/header/indexHeader.js
@@ -1,6 +1,6 @@
 import {memo, useState} from 'react';
 import './styleHeader.scss';
-import { FaFacebook,FaInstagramSquare,FaTiktok,FaRegUser,FaTelegram,FaPhone } from "react-icons/fa";
+import { FaFacebook, FaInstagramSquare, FaTiktok, FaRegUser, FaTelegram, FaPhone } from "react-icons/fa";
 import { AiOutlineShoppingCart } from "react-icons/ai";
 import {Link} from 'react-router-dom';
 import { IoIosMail } from "react-icons/io";
@@ -9,6 +9,7 @@ import { formatter } from 'utils/formater';
 import { ROUTERS } from 'utils/router';
 
 const Header = () => {
+   
     const [isProductList, setProductList] = useState(true);
     const [menus] = useState([
         {
@@ -21,7 +22,7 @@ const Header = () => {
         },
         {
             name: 'Gấu Teddy',
-            path: ROUTERS.USER.HOME,//
+            path: ROUTERS.USER.HOME,
             isShowSubMenu: false,
             child: [
                 {
@@ -40,8 +41,8 @@ const Header = () => {
         },
         {
             name: 'Gấu hoạt hình',
-            path: ROUTERS.USER.HOME,//
-            isShowSubMenu:false,
+            path: ROUTERS.USER.HOME,
+            isShowSubMenu: false,
             child: [
                 {
                     name: 'Gấu bông cho bé',
@@ -59,9 +60,9 @@ const Header = () => {
         },
         {
             name: 'Thú bông',
-            path: ROUTERS.USER.HOME,//
+            path: ROUTERS.USER.HOME,
             isShowSubMenu: false,
-            child:[
+            child: [
                 {
                     name: 'Các loài vật',
                     path: ''
@@ -72,115 +73,111 @@ const Header = () => {
                 }
             ]
         },
-
         {
             name: 'Thông Tin',
-            path: ROUTERS.USER.HOME,//
+            path: ROUTERS.USER.HOME,
         }
-    ])
+    ]);
+
     return (
         <>
-    <div className='header__top'>
-    <div className="container">
-        <div className="row">
-            <div className="col-6 header__top_left">
-                <ul>
-                    <li><IoIosMail />tmhonggg@gmail.com</li>
-                    <li>Miễn phí giao hàng từ {formatter(50000)}</li>
-                </ul>
-                </div>
-            <div className="col-6 header__top_right">
-                <ul>
-                    <li><Link to={'#'}><FaFacebook /></Link></li>
-                    <li><Link to={'#'}><FaInstagramSquare /> </Link></li>
-                    <li><Link to={'#'}><FaTiktok /> </Link></li>
-                    <li><Link to={'#'}> <FaTelegram /> </Link></li>
-                    <li><Link to={'#'}> <FaRegUser /></Link><span>Đăng nhập</span></li>
-                </ul>
-            </div>
-        </div>
-    </div>
-    </div>
-
-  <div className="container">
-  <div className="row">
-        <div className="col-xl-3 ">
-           <div className="header__logo">
-            <h1>Góc Nhà Gấu</h1>
-           </div>
-        </div>
-        <div className="col-xl-6 ">
-        <nav className="header__menu">
-            <ul>
-                {menus?.map((menu, menuKey) => (
-                    <li key={menuKey} className="active">
-                        <Link to={menu?.path}>{menu?.name}</Link>
-                        {
-                            menu.child && (
-                                <ul className='header__menu_child'>
-                                    {menu.child.map((child, childKey) =>(
-
-                                        <li key={`${menuKey}-${childKey}`}>
-                                        <Link to={child.path}>{child.name}</Link>
-                                        </li>
-                                    ))}
-                                    
-                                </ul>
-                            )
-                        }
-                    </li>
-                ))}
-            
-            </ul>
-        </nav>
-        </div>
-        <div className="col-xl-3 ">
-            <div className="header__cart">
-                <ul>
-                    <li><Link to = '#'><AiOutlineShoppingCart /><span>0</span></Link></li>
-                </ul>
-                <div className="header__cart_price">
-                    <span>{formatter(0)}</span>
-                </div>
-            </div>
-        </div>
-    </div>
-  </div>
-  <div className="container">
-    <div className="row header__bottom">
-        <div className="col-lg-3 product__list"> 
-            <div className='product__list_all' onClick = {() => setProductList(!isProductList)}> <IoMenuOutline />Danh sách sản phẩm</div>
-            {
-                isProductList && (
-                <ul >
-                <li><Link to='#'>Gấu Teddy</Link></li>
-                <li><Link to='#'>Gấu Hoạt hình</Link></li>
-                <li><Link to='#'>Thú bông</Link></li>
-                </ul>
-                )
-            }
-            
-             </div>
-        <div className="col-lg-9 header__bottom_search">
-            <div className="search">
-                <div className="search__form">
-                    <form >
-                    <input type="text" placeholder="Tìm kiếm sản phẩm..." />
-                    <button type="submit">Tìm kiếm</button>
-                    </form>
-                </div>
-                <div className="bottom__phone">
-                    <div className="phone__icon"><FaPhone /></div>
-                    <div className="phone__text">
-                        <p>096-562-4836</p>
-                        <span>Hỗ trợ 24/7</span>
+            <div className='header__top'>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-6 header__top_left">
+                            <ul>
+                                <li><IoIosMail />tmhonggg@gmail.com</li>
+                                <li>Miễn phí giao hàng từ {formatter(50000)}</li>
+                            </ul>
+                        </div>
+                        <div className="col-6 header__top_right">
+                            <ul>
+                                <li><Link to={'#'}><FaFacebook /></Link></li>
+                                <li><Link to={'#'}><FaInstagramSquare /> </Link></li>
+                                <li><Link to={'#'}><FaTiktok /> </Link></li>
+                                <li><Link to={'#'}> <FaTelegram /> </Link></li>
+                                <li><Link to={'#'}> <FaRegUser /></Link><span>Đăng nhập</span></li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </div>
-  </div>
-    </>
+
+            <div className="container">
+                <div className="row">
+                    <div className="col-xl-3">
+                        <div className="header__logo">
+                            <h1>Góc Nhà Gấu</h1>
+                        </div>
+                    </div>
+                    <div className="col-xl-6">
+                        <nav className="header__menu">
+                            <ul>
+                                {menus?.map((menu, menuKey) => (
+                                    <li key={menuKey} className="active">
+                                        <Link to={menu?.path}>{menu?.name}</Link>
+                                        {menu.child && (
+                                            <ul className='header__menu_child'>
+                                                {menu.child.map((child, childKey) => (
+                                                    <li key={`${menuKey}-${childKey}`}>
+                                                        <Link to={child.path}>{child.name}</Link>
+                                                    </li>
+                                                ))}
+                                            </ul>
+                                        )}
+                                    </li>
+                                ))}
+                            </ul>
+                        </nav>
+                    </div>
+                    <div className="col-xl-3">
+                        <div className="header__cart">
+                            <ul>
+                                <li><Link to='#'><AiOutlineShoppingCart /><span>0</span></Link></li>
+                            </ul>
+                            <div className="header__cart_price">
+                                <span>{formatter(0)}</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="container">
+                <div className="row header__bottom">
+                    <div className="col-lg-3 product__list">
+                        <div className='product__list_all' onClick={() => setProductList(!isProductList)}>
+                            <IoMenuOutline />Danh sách sản phẩm
+                        </div>
+                        {isProductList && (
+                            <ul>
+                                <li><Link to='#'>Gấu Teddy</Link></li>
+                                <li><Link to='#'>Gấu Hoạt hình</Link></li>
+                                <li><Link to='#'>Thú bông</Link></li>
+                            </ul>
+                        )}
+                    </div>
+                    <div className="col-lg-9 header__bottom_search">
+                        <div className="search">
+                            <div className="search__form">
+                                <form>
+                                    <input type="text" placeholder="Tìm kiếm sản phẩm..." />
+                                    <button type="submit">Tìm kiếm</button>
+                                </form>
+                            </div>
+                            <div className="bottom__phone">
+                                <div className="phone__icon"><FaPhone /></div>
+                                <div className="phone__text">
+                                    <p>096-562-4836</p>
+                                    <span>Hỗ trợ 24/7</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="item"> </div>
+                    </div>
+                </div>
+            </div>
+        </>
     );
 };
 
