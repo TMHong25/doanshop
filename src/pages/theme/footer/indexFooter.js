efc22b6c70da1cb5c5b50b35b53763dbb097f803
Lam Nguyen <lamnguyen1112@gmail.com>
2024-10-26 11:40:04 +0700

    Refactor code

--- src/pages/theme/footer/indexFooter.js
+++ src/pages/theme/footer/indexFooter.js
@@ -10,42 +10,9 @@ const Footer = () => {
         <div className="footer">
             <div className="container">
                 <div className="row">
-                    <div className="col-lg-4">
-                      <div className="footer__about">
-                        <h3 className="footer__about_logo"> Thông tin Liên hệ</h3>
-                        <ul>
-                            <li><FaLocationDot /> Địa chỉ: Tân Lập - Đan Phượng - Hà Nội</li>
-                            <li><FaPhone />Phone: 096-562-4836</li>
-                            <li> <IoIosMail />Email: tmhonggg@gmail.com</li>
-                            <li><FaCalendar />Thời gian: 8h30 - 22h các ngày trong tuần.</li>
-                            
-                        </ul>
-                      </div>
-                    </div>
-                    <div className="col-lg-4">
-                        <div className="footer__widget">
-                            <h3 className='footer__widget_logo'>Chuyển khoản online</h3>
-                            <ul>
-                                <li>Ngân hàng BIDV</li>
-                                <li><FaUser /> Chủ tài khoản: Thế Minh Hồng</li>
-                                <li><MdAccountBalance />Số tài khoản: 116-041-6114</li>
-                              
-                            </ul>
-                           </div>
-                    </div>
-                    <div className="col-lg-4">
-                        <div className="footer__title">
-                            <h3 className="footer__title_logo">Những lý do lên chọn Góc nhà gấu</h3>
-                            <ul>
-                                <li><FaCheck />Hàng ngàn mẫu có sẵn tại
-                                Showroom Hà Nội</li>
-                                <li> <FaCheck />Giá bán lẻ luôn luôn thấp</li>
-                                <li> <FaCheck />Cam kết nhồi 100% bông sạch, độ đàn hồi cao</li>
-                                <li> <FaCheck />Bảo hành gấu 1 năm</li>
-                                <li> <FaCheck />Miễn phí ship khi mua hàng lần 3.</li>
-                            </ul>
-                        </div>
-                        </div>
+                    <ContactInfo />
+                    <PaymentInfo />
+                    <OtherInfo />
                 </div>
             </div>
         </div>
@@ -57,5 +24,55 @@ const Footer = () => {
     
       );
 };
+
+const ContactInfo = () => {
+    return (
+        <div className="col-lg-4">
+        <div className="footer__about">
+          <h3 className="footer__about_logo"> Thông tin Liên hệ</h3>
+          <ul>
+              <li><FaLocationDot /> Địa chỉ: Tân Lập - Đan Phượng - Hà Nội</li>
+              <li><FaPhone />Phone: 096-562-4836</li>
+              <li> <IoIosMail />Email: tmhonggg@gmail.com</li>
+              <li><FaCalendar />Thời gian: 8h30 - 22h các ngày trong tuần.</li>
+              
+          </ul>
+        </div>
+      </div>
+    );
+}
+
+const PaymentInfo = () => {
+    return (
+        <div className="col-lg-4">
+        <div className="footer__widget">
+            <h3 className='footer__widget_logo'>Chuyển khoản online</h3>
+            <ul>
+                <li>Ngân hàng BIDV</li>
+                <li><FaUser /> Chủ tài khoản: Thế Minh Hồng</li>
+                <li><MdAccountBalance />Số tài khoản: 116-041-6114</li>
+            </ul>
+           </div>
+        </div>
+    )
+}
+
+const OtherInfo = () => {
+    return (
+        <div className="col-lg-4">
+            <div className="footer__title">
+                <h3 className="footer__title_logo">Những lý do lên chọn Góc nhà gấu</h3>
+                <ul>
+                    <li><FaCheck />Hàng ngàn mẫu có sẵn tại
+                                Showroom Hà Nội</li>
+                    <li> <FaCheck />Giá bán lẻ luôn luôn thấp</li>
+                    <li> <FaCheck />Cam kết nhồi 100% bông sạch, độ đàn hồi cao</li>
+                    <li> <FaCheck />Bảo hành gấu 1 năm</li>
+                    <li> <FaCheck />Miễn phí ship khi mua hàng lần 3.</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
 
 export default memo(Footer); 
